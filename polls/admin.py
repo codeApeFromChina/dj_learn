a137d013bc838b497838c94b7cad80c16ffc3244
xinghen <xinghen.1991@gmail.com>
2017-06-01 21:02:07 +0800

    django learn 2

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from models import Question, Choice
+from .models import Question, Choice
 
 
 class QuestionAdmin(admin.ModelAdmin):
